88e97af8d7817d46b81d5309277d33c8a0535f1b
CyberHorse <93975433+CyberHorse5021@users.noreply.github.com>
2022-05-28 22:50:36 +0200

    Add files via upload

---
+++ tahmatrice.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+//////////////////////////////////////
+////////////////////////////////////
+
+int** createMat(int *a, int *b){
+
+	do{
+	printf("Input m : ");
+	scanf("%d", &*a);
+	printf("Input n : ");
+	scanf("%d", &*b);
+	}while((*a<1) || (*b<1));
+
+
+	int **tab = (int**)malloc(sizeof(int*)*(*a));
+    for(int i=0;i<*a;i++){
+        tab[i] = (int*)malloc((*b) * sizeof(int));
+    }
+	
+	return tab;
+}
+
+//////////////////////////////////////
+////////////////////////////////////
+
+void fillIn(int** tab, int* m,int* n){
+	
+	int k = 0;
+	for(int i=0;i<*m;++i){
+		for(int j=0;j<*n;++j){
+			tab[i][j]=rand()%256;
+		}
+	}
+
+}
+
+//////////////////////////////////////
+////////////////////////////////////
+
+void displayMat(int**tab, int* m, int* n){
+	printf("Result : \n\n");
+	for(int i=0;i<*m;++i){
+		for(int j=0;j<*n;++j){
+			printf("%d  ", tab[i][j]);
+		}
+		printf("\n");
+	}
+}
+
+//////////////////////////////////////
+////////////////////////////////////
+
+
+int main(){
+	
+	srand(time(NULL));
+	int a;
+    int b;
+	int** mat;
+	
+	mat = createMat(&a,&b);
+	fillIn(mat, &a,&b);
+	displayMat(mat, &a,&b);
+
+	return 0;
+}
