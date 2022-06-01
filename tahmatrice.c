b80355f0e065ede9a2cbc1bfd9b0c8c6c3e5be48
CyberHorse <93975433+CyberHorse5021@users.noreply.github.com>
2022-06-01 23:12:50 +0200

    Add files via upload

--- tahmatrice.c
+++ tahmatrice.c
@@ -31,10 +31,9 @@ void fillIn(int** tab, int* m,int* n){
 	int k = 0;
 	for(int i=0;i<*m;++i){
 		for(int j=0;j<*n;++j){
-			tab[i][j]=rand()%256;
+			tab[i][j]=rand()%255;
 		}
 	}
-
 }
 
 //////////////////////////////////////
